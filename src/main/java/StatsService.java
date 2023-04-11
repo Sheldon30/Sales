2856f015bc0e947191035504ae763e6ecd429a7d
Sheldon <aneln700@gmail.com>
2023-04-11 22:21:11 +0300

    Final

--- src/main/java/StatsService.java
+++ src/main/java/StatsService.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class StatsService {
+public class StatsService {
+    public long findsaleSum(long[] sale) {
+        long saleSum = 0;
+        for (long i : sale) {
+            saleSum += i;
+        }
+        return saleSum;
+    }
+
+    public long findaveramount(long[] sale) {
+        long averamount = 0;
+        for (long i : sale) {
+            averamount += i;
+
+        }
+        return averamount / sale.length;
+
+
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж больше
+                maxMonth = i; // запомним его как максимальный
+            }
+        }
+
+        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int findUnderAveramount(long[] sales) {
+        long minSaleMonth = 0;
+        long average = findaveramount(sales);
+        for (long sale: sales) {
+            if (sale< average) {
+                minSaleMonth++;
+            }
+        }
+        return (int) minSaleMonth;
+    }
+
+    public int findHigherAveramount(long[] sales) {
+        long maxSaleMonth = 0;
+        long average = findaveramount(sales);
+        for (long sale: sales) {
+            if (sale< average) {
+                maxSaleMonth++;
+            }
+        }
+        return (int) maxSaleMonth;
+    }
 }
+
+
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class StatsServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void findSumSale() {
+        StatsService service = new StatsService();
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedSale = 180;
+        long actualSale = service.findsaleSum(sale);
+        Assertions.assertEquals(expectedSale, actualSale);
+
+    }
+
+    @Test
+
+    public void findaverAmount() {
+        StatsService service = new StatsService();
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedAverSale = 15;
+
+        long actualAverSale = service.findaveramount(sale);
+        Assertions.assertEquals(expectedAverSale, actualAverSale);
+
+    }
+
+    @Test
+
+    public void findMonthMaxSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedMax = 8;
+        long actualMax = service.maxSales(sales);
+        Assertions.assertEquals(expectedMax, actualMax);
+
+    }
+
+    @Test
+
+    public void findMonthMinSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedMin = 9;
+        long actualMin = service.minSales(sales);
+        Assertions.assertEquals(expectedMin, actualMin);
+    }
+
+    @Test
+    public void findMonthUnderSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedMinMonth = 5;
+        long actualMinMonth = service.findUnderAveramount(sales);
+        Assertions.assertEquals(expectedMinMonth, actualMinMonth);
+    }
+
+    @Test
+    public void findMonthHigherSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedMaxMonth = 5;
+        long actualMaxMonth = service.findHigherAveramount(sales);
+        Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
+    }
 }
